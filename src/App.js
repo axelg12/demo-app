b2ab4b5b89d8b9bfeeceb1a505d6b6e9cc67d4a5
Axel Mani <selii93@gmail.com>
2018-02-02 23:56:31 +0100

    Demo to show

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ class App extends Component {
       <div className="App">
         <div className="App__container">
           <div className="Container">
-            <div class="Container__text">LANDSCAPE ARCHITECTURE</div>
+            <div class="Container__text">
+              <div>ICELANDIC LANDSCAPE</div>
+              <div> VISUAL ELEMENT</div>
+            </div>
+          </div>
+          <div className="InfoBox">
+            <div className="InfoBox__text">
+              <div className="InfoBox__textTitle">Litatónar</div>
+              Meginmarkmið þessa verkefnis er að varpa ljósi á sjónræna þætti og
+              myndmál landslagsins sem umlykur okkur dags daglega. Mikilvægi
+              vitundar um heimabyggð og tilfinning fyrir nærumhverfi eru þættir
+              sem koma fram bæði í sjálfbærnimarkmiðum Sameinuðu þjóðanna og
+              nýrri aðal- námskrá leik-, grunn- og framhaldsskóla. Með því að
+              styrkja tilfinningu fyrir lífræn- um ferlum, breytingum í lit og
+              formi er verkefnið innlegg í kennslu námsmarkmiða í umhverfismennt
+              og sjónlistum.
+            </div>
+            <div className="InfoBox__colors" />
           </div>
         </div>
       </div>
